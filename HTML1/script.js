1cd51cd07cd8df74ac072b5521dd6d15644e3d96
Lera Yurkova <lerahand@mail.ru>
2021-03-16 16:32:45 +0300

    add

---
+++ HTML1/script.js
@@ -0,0 +1,26 @@
+class Service {
+
+    Posts = [
+        {
+            id: '1',
+            descriprion: 'Талон',
+            createdAt: new Date('2021-03-5T23:00:00'),
+            author: 'ИвановИван',
+            photoLink: 'http://fpmi.bsu.by/ImgFpmi/Cache/banner_61403.jpg'
+        },
+        {
+            id: '2',
+            descriprion: 'Талон',
+            createdAt: new Date('2021-03-5T23:00:00'),
+            author: 'ИвановИван',
+            photoLink: 'http://fpmi.bsu.by/ImgFpmi/Cache/banner_61403.jpg'
+        }
+    ];
+
+    getPost(id ){
+        for()
+    }
+
+}
+
+let newBlock=document.querySelector('.new-block-text');
